b7870deaecba5e59cf25a736795eeb735fcd020f
Angelikam <an_lev@yahoo.com>
2023-01-19 17:31:26 +0300

    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+
+if(result == 0)
+{
+   System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("нечетное");
+}
