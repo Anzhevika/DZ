5059a519511a1f18f42d8a23047e50c6891fbf18
Angelikam <an_lev@yahoo.com>
2023-01-19 17:29:29 +0300

    Lesson4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите пожалуйста число : ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int num = 1;
+
+while (num <= numN)
+{
+    if(num % 2 == 0)
+    {
+        System.Console.WriteLine(num);
+        num++;
+    }
+    else
+    {
+        num++;
+    }
+
+}
+
